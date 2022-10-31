fcddb185d4a51fd83715791609b92231b07b05fa
KENNEDY <kennedyeziechina@gmail.com>
2022-10-31 02:15:47 +0100

    Static Files Issues

--- config/settings/development.py
+++ config/settings/development.py
@@ -9,8 +9,11 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-MEDIA_ROOT = BASE_DIR / 'media/'
-STATIC_ROOT = BASE_DIR / '/home/vodatro1/educare-rfid.vodatrox.com/static'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Gathers the static files here when collect static command is run
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Normally Look for the static files here Collect Static
+
+
+MEDIA_ROOT = BASE_DIR / 'media/'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
--- config/settings/production.py
+++ config/settings/production.py
@@ -12,9 +12,12 @@ ALLOWED_HOSTS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-MEDIA_ROOT = BASE_DIR / 'media/'
-STATIC_ROOT = BASE_DIR / '/home/vodatro1/educare-rfid.vodatrox.com/static'
+# Gathers the static files here when collect static command is run
+STATIC_ROOT = BASE_DIR / 'home/vodatro1/educare-rfid.vodatrox.com/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Normally Look for the static files here Collect Static
 
+
+MEDIA_ROOT = BASE_DIR / 'media/'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
